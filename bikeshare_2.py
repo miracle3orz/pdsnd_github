368295ad9edf377223f104a35f575a139f6507a0
miracle <769998701@qq.com>
2023-04-17 14:12:58 +0200

    change function Name

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -156,7 +156,7 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
-def display_raw_data(df):
+def display_raw(df):
     """ Your docstring here """
     i = 0
     raw = input("\nDo you want to view the data? Enter yes or no.\n").lower()
@@ -182,7 +182,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        display_raw_data(df)
+        display_raw(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() == "no":
